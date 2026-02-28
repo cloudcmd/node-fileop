7be17c9f9bc8c8d67bae9843485e7f037f94adfd
coderaiser <coderaiser@users.noreply.github.com>
2026-02-28 18:10:15 +0000

    chore: @cloudcmd/fileop: actions: lint ☘️

--- client/load-socket.spec.js
+++ client/load-socket.spec.js
@@ -5,7 +5,7 @@ test('fileop: client: load-socket: no io', async (t) => {
     globalThis.io = 'x';
     const io = await loadSocket();
     delete globalThis.io;
-
+    
     t.equal(io, 'x');
     t.end();
-})
+});
